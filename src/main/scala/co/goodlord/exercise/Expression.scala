2579b1334956f1ceb485e580f2e194fa85ba8ab4
Anton Vanco <anton.vanco@magnetic.com>
2017-09-02 22:48:38 +0100

    Minor performance improvements

--- src/main/scala/co/goodlord/exercise/Expression.scala
+++ src/main/scala/co/goodlord/exercise/Expression.scala
@@ -9,15 +9,15 @@ case class Operand(value: Int) extends Expression {
 sealed trait Operator extends Expression
 
 case class Addition(left: Expression, right: Expression) extends Operator {
-  def value: Int = left.value + right.value
+  val value: Int = left.value + right.value
   override def toString: String = s"($left + $right)"
 }
 case class Subtraction(left: Expression, right: Expression) extends Operator {
-  def value: Int = left.value - right.value
+  val value: Int = left.value - right.value
   override def toString: String = s"($left - $right)"
 }
 case class Multiplication(left: Expression, right: Expression) extends Operator {
-  def value: Int = left.value * right.value
+  val value: Int = left.value * right.value
   override def toString: String = s"($left * $right)"
 }
 
@@ -30,16 +30,12 @@ object Expression {
       case Some(r) if r.value < 0 =>
         None
       case Some(exp) =>
-        op.flatMap { o =>
+        op.map { o =>
           val remainder = op.filter(_ != o)
           val add = Addition(exp, o)
           val sub = Subtraction(exp, o)
           val mult = Multiplication(exp, o)
-          Seq(
-            go(remainder, Some(add)),
-            go(remainder, Some(sub)),
-            go(remainder, Some(mult))
-          )
+          go(remainder, Some(add)).orElse(go(remainder, Some(sub))).orElse(go(remainder, Some(mult)))
         }.collectFirst { case Some(res) => res }
       case None =>
         op.map { o =>
@@ -57,6 +53,6 @@ object Main {
   import Expression._
   def main(args: Array[String]): Unit = {
     println(Expression(42, Seq(2, 3, 5, 6)))
-    println(Expression(128, Seq(1, 2, 3, 4, 5, 6, 7)))
+    println(Expression(128, Seq(1, 2, 3, 4, 5, 6, 7, 8)))
   }
 }
